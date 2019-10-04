3c03cdcf6f8e2f6dfed175de68b0089f78b53722
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-04 15:36:44 -0300

    Criada função para dividir uma frase em um vetor e cada palavra em um vetor

--- HenriqueEyer/script.js
+++ HenriqueEyer/script.js
@@ -2,12 +2,27 @@
 
 
 
+
+
+function split_frase(txt){
+    let split_str=txt.split(" ");
+    return split_str;
+}
+
+
+function add_event_click_btn_input(){
+    return_btn_input().addEventListener('click', function(){
+        //Chamar função para seperar o texto digitado em um vetor com cada palavra
+        //Chamar funçao para chamar as divs ou cria las
+
+    })
+}
+
+
 function return_btn_input(){
     let btn_input=document.getElementById('btn_input')
     return btn_input;
 }
-
-
 
 function get_txt_input(){
     let txt_input=document.getElementById('txt_input')
