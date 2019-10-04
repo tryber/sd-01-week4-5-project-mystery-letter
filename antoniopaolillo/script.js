4dc3b4b91f2a661e27b9802574f2c3417c30e5e0
antoniopaolillo <antonio_francisco_p@live.com>
2019-10-04 18:55:48 -0300

    deu ruim

--- antoniopaolillo/script.js
+++ antoniopaolillo/script.js
@@ -1,9 +1,7 @@
 var imgs = ["img0.png", "img1.png", "img2.png", "img3.png"];
 var fonts = ["initial", "italic", "inherit", "normal", "oblique", "unset"]
-var cartaMisteriosa = document.getElementsByClassName("cartamisteriosa")[0];
 
-var numCartas = document.getElementsByTagName('div');
-var numCartasLength = numCartas.length - 2;
+var cartaMisteriosa = document.getElementsByClassName("cartamisteriosa")[0];
 
 var textoDigitado = document.getElementById("inputtxt");
 var textoDigitadoValue = textoDigitado.value;
@@ -13,8 +11,11 @@ var simbolos = "0123456789ABCDEF";
 var colorBackground = "#";
 var colorBackground2 = "#";
 
-function gerarcartas() {
+var carta;
 
+function gerarcartas() {
+    var numCartas = document.getElementsByTagName('div');
+    var numCartasLength = numCartas.length - 2;
 
     //apagando cartas anteriores
     for (let j = 0; j < numCartasLength; j++) {
@@ -24,7 +25,7 @@ function gerarcartas() {
 
     //gerando o numero de cartas
     for (let i = 0; i < textoDigitadoSplit.length; i++) {
-        var carta = document.createElement('div');
+        carta = document.createElement('div');
         carta.setAttribute("class", "cartas");
         cartaMisteriosa.appendChild(carta);
 
@@ -37,21 +38,21 @@ function gerarcartas() {
         buscandoimg();
         stylefonte();
     }
+
 }
 
-
-    
-    function corbackground(){
-    //gerando backgroundcolor
+//gerando backgroundcolor
+function corbackground() {
     for (let n = 0; n < 6; n++) {
         let simboloSorteado = simbolos[Math.floor(Math.random() * 16)];
         colorBackground = colorBackground + simboloSorteado;
     }
     carta.style.backgroundColor = colorBackground;
     colorBackground = "#";
-}   
-    function geracor(){
-    //gerando cor da fonte
+}
+
+//gerando cor da fonte
+function geracor() {
     for (let n = 0; n < 6; n++) {
         let simboloSorteado2 = simbolos[Math.floor(Math.random() * 16)];
         colorBackground2 = colorBackground2 + simboloSorteado2;
@@ -59,20 +60,39 @@ function gerarcartas() {
     carta.style.color = colorBackground2;
     colorBackground2 = "#";
 }
-    //gerando tamanho da fonte
-    function tamanhofonte(){
+
+//gerando tamanho da fonte
+function tamanhofonte() {
     var tamanhoFonte = Math.floor(Math.random() * (70 - 20) + 20);
     carta.style.fontSize = tamanhoFonte + 'px';
-    }
+}
 
-    function buscandoimg(){
-    //buscando background img
+//buscando background img
+function buscandoimg() {
     let sorteioImagem = Math.floor(Math.random() * 4);
-    carta.style.backgroundImage = "url('img"+[sorteioImagem]+".png')"
-    }
-    
-    function stylefonte(){
-    //gerando style da fonte
+    carta.style.backgroundImage = "url('img" + [sorteioImagem] + ".png')"
+}
+
+//gerando style da fonte
+function stylefonte() {
     let sorteioStyle = Math.floor(Math.random() * 6);
     carta.style.fontStyle = fonts[sorteioStyle];
-    }
+}
+
+//alterando fontes mediante clique
+// var numeroCartas = document.getElementsByTagName('div')
+// let teste2 = numeroCartas.length;
+// console.log(numeroCartas)
+// console.log(teste2);
+
+// for(let u = 0; u < numCartasLength; u++){
+//     var cartasMudando = document.getElementsByClassName("cartas")[u];
+//     console.log(cartasMudando)
+//     cartasMudando.addEventListener('click', function(){
+//         corbackground();
+//         geracor();
+//         tamanhofonte();
+//         buscandoimg();
+//         stylefonte();
+//     })
+// }
