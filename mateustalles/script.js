e001dbf4528e971e7c1b79e4236eb64105b9af4b
Mateus Talles <mateustalles@gmail.com>
2019-10-07 17:13:33 -0300

    Aplicada a primeira função de exibir o texto na pagina

--- mateustalles/script.js
+++ mateustalles/script.js
@@ -1,2 +1,9 @@
-let letterContainer = document.querySelector(".input-letter"),
-    letter = letterContainer.value;
+//função gera texto em div a partir do input.
+
+let input = document.querySelector(".input-letter");
+input.addEventListener("change", function() {
+    let letterContainer = document.querySelector(".container-result");
+    letterContainer.innerText=input.value;
+});
+
+//função tamanho de letra aleatório
