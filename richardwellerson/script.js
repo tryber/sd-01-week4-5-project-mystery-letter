f8a0d1889a9275e35d1610e01300686189b54470
Richard Wellerson <richardwellerson@gmail.com>
2019-10-09 18:24:06 -0300

    terminado a parte de JS alterando as cores e bg das palavras. Terminando detalhes de tamanho.

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -1,3 +1,5 @@
+listaSpan = document.getElementsByClassName("palavrasatrabalhar")
+
 function botaoFunc(){
     let botao = document.querySelector(".botao")
     botao.addEventListener('click', function(){
@@ -19,18 +21,22 @@ function criarTagSpan(text){
         palavraSeparada.innerHTML = text[i];
         let capturarDiv = document.querySelector(".receberpalavras");
         capturarDiv.appendChild(palavraSeparada);
+        determinarEstilo()
     }
 
 }
 
 function determinarEstilo(){
-    for(let i = 0; i < arraySepPalavras.length; i++){
+    for(let i = 0; i < listaSpan.length; i++){
         let r = Math.floor(Math.random() * 256);
         let g = Math.floor(Math.random() * 256);
         let b = Math.floor(Math.random() * 256);
         let backgroundColorArray = "rgb(" + r +", " + g + ", " + b +")";
         let fontColor = "rgb(" + g +", " + b + ", " + r +")";
         let wordSize = Math.floor((Math.random()+20) * 3)+"px";
+        listaSpan[i].style.backgroundColor = backgroundColorArray;
+        listaSpan[i].style.color = fontColor
+        listaSpan[i].font_Size = wordSize
     }
 }
 
