86de04e995e1190b027ed66395b34ad0c207fe37
Richard Wellerson <richardwellerson@gmail.com>
2019-10-09 14:01:41 -0300

    Adicionando detalhes de teste no .css, testando o recebimento e tamanho dos pixels, para calcular o valor certo de multiplicaçao no js. Construindo o for de random style no texto.

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -4,15 +4,21 @@ function capturarTexto() {
     event.preventDefault()
     let valorEscrito = document.querySelector(".jscaptura").value
     let arraySepPalavras = valorEscrito.split(" ")
-    console.log(valorEscrito)
-    console.log(arraySepPalavras)
     }
-    )}
+)}
 
     capturarTexto()
 
 for(i of arraySepPalavras){
-    bgColor = 
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let backgroundColorArray = "rgb(" + r +", " + g + ", " + b +")";
+    let fontColor = "rgb(" + g +", " + b + ", " + r +")";
+    let wordSize = Math.floor((Math.random()+20) * 3)+"px";
+    arraySepPalavras[i].style.backgroundColor = backgroundColorArray;
+    arraySepPalavras[i].style.color = fontColor;
+    arraySepPalavras[i].style.fontSize = wordSize;
 }
 
 
