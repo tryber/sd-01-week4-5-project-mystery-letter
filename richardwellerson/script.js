165c17bef0b5f7e495fd28ab360533f1df6bf3b1
Richard Wellerson <richardwellerson@gmail.com>
2019-10-09 20:37:50 -0300

    Adicionada as imagens e trabalhado o easter Egg

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -1,21 +1,22 @@
 listaSpan = document.getElementsByClassName("palavrasatrabalhar")
 
-function botaoFunc(){
+function botaoFunc() {
     let botao = document.querySelector(".botao")
-    botao.addEventListener('click', function(){
+    botao.addEventListener('click', function () {
         criarTagSpan(capturarTexto())
     }
-)}
+    )
+}
 
-function capturarTexto(){
+function capturarTexto() {
     let valorEscrito = document.querySelector(".jscaptura").value
     let arraySepPalavras = valorEscrito.split(" ")
     console.log(arraySepPalavras)
     return arraySepPalavras;
 }
 
-function criarTagSpan(text){
-     for(let i = 0; i < text.length; i++){
+function criarTagSpan(text) {
+    for (let i = 0; i < text.length; i++) {
         let palavraSeparada = document.createElement("span");
         palavraSeparada.classList.add("palavrasatrabalhar")
         palavraSeparada.innerHTML = text[i];
@@ -23,25 +24,41 @@ function criarTagSpan(text){
         capturarDiv.appendChild(palavraSeparada);
         determinarEstilo()
     }
-
 }
 
-function determinarEstilo(){
-    for(let i = 0; i < listaSpan.length; i++){
+function determinarEstilo() {
+    for (let i = 0; i < listaSpan.length; i++) {
         let r = Math.floor(Math.random() * 256);
         let g = Math.floor(Math.random() * 256);
         let b = Math.floor(Math.random() * 256);
-        let backgroundColorArray = "rgb(" + r +", " + g + ", " + b +")";
-        let fontColor = "rgb(" + g +", " + b + ", " + r +")";
-        let wordSize = Math.floor((Math.random()+20) * 3)+"px";
-        listaSpan[i].style.backgroundColor = backgroundColorArray;
-        listaSpan[i].style.color = fontColor
-        listaSpan[i].font_Size = wordSize
+        let fontColor = "rgb(" + g + ", " + b + ", " + r + ")";
+        let wordSize = Math.floor((Math.random(1)+2) * 50) + "px";
+        let backgroundColorArray = "rgb(" + r + ", " + g + ", " + b + ")";
+        let imgBg = "url('imagesAndMusic/" + Math.floor(Math.random() * i) + ".png')"
+        console.log(imgBg)
+        if (i % 2 == 0) {
+            listaSpan[i].style.backgroundImage = imgBg;
+            listaSpan[i].style.color = fontColor;
+            listaSpan[i].style.fontSize = wordSize;
+        } else {
+            listaSpan[i].style.backgroundColor = backgroundColorArray;
+            listaSpan[i].style.color = fontColor;
+            listaSpan[i].style.fontSize = wordSize;
+        }
+        }
+}
+
+
+function apagaTags() {
+    let qtdSpan = document.getElementsByClassName("receberpalavras");
+    let qtdTagsSpan = document.getElementsByTagName("span");
+    for (let i = 0; i <= qtdTagsSpan.length; i++) {
+        qtdSpan.removeChild(qtdSpan.firstChild())
     }
 }
 
-    botaoFunc()
-    
+botaoFunc()
+
 
 
 
