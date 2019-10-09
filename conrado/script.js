60ab5364db059e431e4ea63d40d33d7b9de3984b
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-10-09 17:20:38 -0300

    textSplit[arr] finish

--- conrado/script.js
+++ conrado/script.js
@@ -3,7 +3,14 @@ btn.addEventListener("click", function clickMouseLocation() {
     return showText();
 }) 
 function showText() {
-    let text1 = document.getElementById('typeText').value;
+    text1 = document.getElementById('typeText').value;
     let showText = document.getElementById('showText');
     showText.innerHTML = text1;
+    return textSplit(text1);
+    
+}
+function textSplit(text1){
+    text1 = document.getElementById('typeText').value;
+    textSplit1 = text1.split(" ");
+    console.log(textSplit1[0]);
 }
