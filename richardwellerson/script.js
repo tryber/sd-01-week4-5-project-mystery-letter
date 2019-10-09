f194676b36af806b36c68c5ec7dfbff650e882c9
Richard Wellerson <richardwellerson@gmail.com>
2019-10-09 17:09:45 -0300

    Alterando estrutura e logica de trabalhar no projeto. Modificando forma de trabalhar com blocos para inline, alterando js para funçoes por tipo e ajustando css para testes.

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -1,25 +1,38 @@
-function capturarTexto() {
-    botao = document.querySelector(".botao")
-    botao.addEventListener('click', function(event){
-    event.preventDefault()
-    let valorEscrito = document.querySelector(".jscaptura").value
-    let arraySepPalavras = valorEscrito.split(" ")
+function botaoFunc(){
+    let botao = document.querySelector(".botao")
+    botao.addEventListener('click', function(){
+        capturarTexto()
     }
 )}
 
-    capturarTexto()
-
-for(i of arraySepPalavras){
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    let backgroundColorArray = "rgb(" + r +", " + g + ", " + b +")";
-    let fontColor = "rgb(" + g +", " + b + ", " + r +")";
-    let wordSize = Math.floor((Math.random()+20) * 3)+"px";
-    arraySepPalavras[i].style.backgroundColor = backgroundColorArray;
-    arraySepPalavras[i].style.color = fontColor;
-    arraySepPalavras[i].style.fontSize = wordSize;
+function capturarTexto(){
+    let valorEscrito = document.querySelector(".jscaptura").value
+    let arraySepPalavras = valorEscrito.split(" ")
+    return arraySepPalavras;
 }
+
+function criarTagSpan(){
+     for(i = 0; i < capturarTexto().lenght; i++)
+        let divFeita = document.createElement("div");
+        divFeita.innerHTML= arraySepPalavras[x];
+        document.div.appendChild(divFeita);
+        divFeita.classList.add("divjsfeita");
+}
+
+function determinarEstilo(){
+    for(i = 0; i < arraySepPalavras.length; i++){
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        let backgroundColorArray = "rgb(" + r +", " + g + ", " + b +")";
+        let fontColor = "rgb(" + g +", " + b + ", " + r +")";
+        let wordSize = Math.floor((Math.random()+20) * 3)+"px";
+    }
+}
+
+    botaoFunc()
+    
+
 
 
 
