5f4cbc4d0bcb4f31d897ad688f7404e4ec2da014
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-10-09 16:50:45 -0300

    button javascript finish,  showText

--- conrado/script.js
+++ conrado/script.js
@@ -1,4 +1,9 @@
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function clickMouseLocation() {
-    console.log("oi");
+    return showText();
 }) 
+function showText() {
+    let text1 = document.getElementById('typeText').value;
+    let showText = document.getElementById('showText');
+    showText.innerHTML = text1;
+}
